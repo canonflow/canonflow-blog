478072d1b1fdd940f641fa504632b9b0e828bdab
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2023-07-02 21:04:33 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
