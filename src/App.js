ce9b38314d12bde3cb65ea80b8b9590cf6a18b92
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2023-07-05 14:21:27 +0700

    React Route (using react-route-dom@6)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 import React from 'react';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter, Route , Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <div className="content">
-        <Home />
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path='/' element={ <Home /> }></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
